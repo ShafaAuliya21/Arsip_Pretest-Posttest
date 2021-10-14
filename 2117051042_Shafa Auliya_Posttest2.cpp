ed33b0fb98cacbd1091965f545f3b940cd12a194
Shafa Auliya <92239898+ShafaAuliya21@users.noreply.github.com>
2021-10-14 17:34:58 +0700

    Add files via upload

---
+++ 2117051042_Shafa Auliya_Posttest2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+// Nama  : Shafa Auliya
+// Kelas : A
+// NPM   : 2117051042
+
+   cout << "Posttest 2 Praktikum Dasar Dasar Pemograman"<< endl;
+   cout << "Program Menghitung Nilai Dari Fungsi ax^2 + bx + c" << endl;
+   
+   float a;
+   float b;
+   float c;
+   float x;
+   
+   //input
+   cout << "a,b,c,x : ";
+   cin >> a >> b >> c >> x;
+
+   //output
+   cout << "Hasil Penjumlahan dari ax^2 + bx + c: "<<(a*(x*x))+ (b*x) + c<< endl;
+} 
