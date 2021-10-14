2e96641ea867efb6f5e51bfccae8ee378a33f944
Shafa Auliya <92239898+ShafaAuliya21@users.noreply.github.com>
2021-10-14 17:32:50 +0700

    Add files via upload

---
+++ 2117051042_Shafa Auliya_Posttest1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string a;
+	string b;
+	string c;
+	
+	cout<< "Nama    : Shafa Auliya" <<endl; 
+	cout<< "Kelas   : A" <<endl;
+	cout<< "NPM     : 2117051042"<<endl;
+	cout<< "Jurusan : S1 Ilmu Komputer"<<endl;
+	cout<< "Pretest 1 Dasar Dasar Pemograman" <<endl;
+		
+	//input
+	cout<<"a,b,c : ";
+	cin>>a>>b>>c;
+		
+	//output
+	cout<<a<<"x^2 + "<< b <<"x + "<< c;
+	return 0;
+}
